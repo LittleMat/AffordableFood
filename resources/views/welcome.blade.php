3e986a1b41351729e8dff5b37af28c3e75bb8fd9
Mathieu Montgomery <mathieu.montgomery@etu.unistra.fr>
2018-04-04 15:58:59 +0200

    Change welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel TEST
+                    Laravel TEST 2
                 </div>
 
                 <div class="links">
