7232e0389001a97bbf46434712e5a804aa462362
Remy Buitenkamp <rbuitenkamp@live.nl>
2018-06-01 07:08:57 +0200

    homepagev1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,100 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <header class="py-1 bg-image-full" style="background-image: url('https://superfoodsrx.com/wp-content/uploads/2016/09/Supermarket_shopping_Study.jpg');">
+    	<div class="toppadding"></div>
+      <img class="img-fluid d-block mx-auto homepage_img1" src="..\image\logo.png" alt="">
+      	<div class="bottompadding"></div>
+    </header>
 
-                    You are logged in!
-                </div>
-            </div>
+    <section class="py-5">
+      <div class="container">
+      	<div class="centertext">
+        <h1>Affordable Food</h1>
+        <p class="lead">Find the cheapest food in Horsens</p>
+        <p>For students and made by students</p>
+    	</div>
+      </div>
+    </section>
+
+    <hr>
+
+    <div class="container-fluid">
+
+	<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 project wow animated animated4 fadeInLeft">
+        <div class="project-hover">
+        	<h2>Find products</h2>
+            <hr />
+            <p>Here you can find where you can buy the cheapest products in the supermarkets of Horsens</p>
+            <a href="#">Search products</a>
         </div>
     </div>
+	<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 project project-2 wow animated animated3 fadeInLeft">
+    	<div class="project-hover">
+        	<h2>Join our Community</h2>
+            <hr />
+            <p>Become a member of our community!</p>
+            <a href="#">Become a Member</a>
+        </div>
+    </div>
+	<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 project project-3 wow animated animated2 fadeInLeft">
+    	<div class="project-hover">
+        	<h2>TITLE</h2>
+            <hr />
+            <p></p>
+            <a href="#">Go To</a>
+        </div>
+    </div>
+	<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 project project-4 wow animated fadeInLeft">
+    	<div class="project-hover">
+        	<h2>Contact Us</h2>
+            <hr />
+            <p>Contact us by sending an E-mail</p>
+            <a href="#">Send E-mail</a>
+        </div>
+    </div>
+    <div class="clearfix"></div>
 </div>
+
+
+{{-- <section class="py-5">
+    <div class="container">
+	  <div class="row">
+	    <div class="col-sm">
+	    	<div class="card homepagecard">
+	      		<img class="img-fluid d-block mx-auto homepage_img2" src="http://www.rivieramayagroceries.com/wp-content/uploads/2018/01/gro.jpg" alt="">
+  				<div class="card-body">
+					<h2 class="card-title">Find products</h2>
+					<p class="card-text">Here you can find where you can buy the cheapest products in the supermarkets in Horsens</p>
+					<a href="#" class="btn btn-primary">Search products</a>
+ 				</div>
+			</div>
+	    </div>
+	    
+	    <div class="col-sm">
+	    	<div class="card homepagecard">
+	      	    <img class="img-fluid d-block mx-auto homepage_img2" src="https://i.imgur.com/OSiAMNo.png" alt="">
+  				<div class="card-body">
+    				<h2 class="card-title">Community</h2>
+    				<p class="card-text">Become a member of our community!</p>
+    				<a href="#" class="btn btn-primary">Buy membership</a>
+				</div>
+			</div>
+	    </div>
+	    
+	    <div class="col-sm">
+			<div class="card homepagecard">
+	      		<img class="img-fluid d-block mx-auto homepage_img2" src="http://www.blackcoffer.com/assets/images/email.png" alt="">
+  				<div class="card-body">
+    				<h5 class="card-title">Contact</h5>
+    				<p class="card-text">Contact us by sending an E-Mail</p>
+   					<a href="#" class="btn btn-primary">Contact</a>
+  				</div>
+			</div>
+	    </div>
+	  </div>
+	</div>
+</section> --}}
+
 @endsection
